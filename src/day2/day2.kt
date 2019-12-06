37ce42abf026c5db688a1813f8352599667f9b8b
alan simon <wolfishflow@users.noreply.github.com>
2019-12-05 23:12:10 -0500

    Finished Day 2 part2 and addressed some todos

--- src/day2/day2.kt
+++ src/day2/day2.kt
@@ -25,7 +25,8 @@ import java.io.File
 const val ADDITION = 1
 const val MULTIPLY = 2
 const val TERMINATE = 99
-const val PROGRAM_SET = 4
+const val NUMBER_OF_VALUES = 4
+const val OBJECTIVE = 19690720
 
 const val test1Input = "1,9,10,3,2,3,11,0,99,30,40,50"
 const val test1Result = "3500,9,10,70,2,3,11,0,99,30,40,50"
@@ -44,55 +45,80 @@ const val test5Result = "30,1,1,4,2,5,6,0,99"
 
 fun main() {
     println("Running Tests:")
-    checkInput(test1Input.split(",").map { it.toInt() }.toIntArray(), test1Result.split(",").map { it.toInt() }.toIntArray())
-    checkInput(test2Input.split(",").map { it.toInt() }.toIntArray(), test2Result.split(",").map { it.toInt() }.toIntArray())
-    checkInput(test3Input.split(",").map { it.toInt() }.toIntArray(), test3Result.split(",").map { it.toInt() }.toIntArray())
-    checkInput(test4Input.split(",").map { it.toInt() }.toIntArray(), test4Result.split(",").map { it.toInt() }.toIntArray())
-    checkInput(test5Input.split(",").map { it.toInt() }.toIntArray(), test5Result.split(",").map { it.toInt() }.toIntArray())
+    checkInput(
+        test1Input.split(",").map { it.toInt() }.toIntArray(),
+        test1Result.split(",").map { it.toInt() }.toIntArray()
+    )
+    checkInput(
+        test2Input.split(",").map { it.toInt() }.toIntArray(),
+        test2Result.split(",").map { it.toInt() }.toIntArray()
+    )
+    checkInput(
+        test3Input.split(",").map { it.toInt() }.toIntArray(),
+        test3Result.split(",").map { it.toInt() }.toIntArray()
+    )
+    checkInput(
+        test4Input.split(",").map { it.toInt() }.toIntArray(),
+        test4Result.split(",").map { it.toInt() }.toIntArray()
+    )
+    checkInput(
+        test5Input.split(",").map { it.toInt() }.toIntArray(),
+        test5Result.split(",").map { it.toInt() }.toIntArray()
+    )
 
-    println("Running Part 1:")
-    val commandInputs = File("src/day2/input").readText().split(",").map { it.trim().toInt() }.toIntArray()
+    println("Running Day 2: ")
+    val commandInputs = File("src/day2/input").readText().split(",").map {
+        it.trim().toInt()
+    }.toIntArray()
 //    replace position 1 with the value 12
     commandInputs[1] = 12
 //    replace position 2 with the value 2
     commandInputs[2] = 2
 //    What value is left at position 0
-    println(initializeWithCommandInputs(commandInputs)[0]) //4090701
+    println("Part 1: ${initializeWithCommandInputs(commandInputs.copyOf())[0]}") //4090701
+    println("Part 2: ${determineNounAndVerb(commandInputs.copyOf())}")//6421
 }
-
 
 
 fun checkInput(commandsIntArray: IntArray, results: IntArray) {
-    println("Input: ${commandsIntArray.asList()}, Result: ${results.asList()}")
+    print("Input: ${commandsIntArray.asList()}, Result: ${results.asList()} ; ")
     println("Did Test Pass: ${initializeWithCommandInputs(commandsIntArray).contentEquals(results)}")
 }
 
-fun initializeWithCommandInputs(commandsIntArray: IntArray) : IntArray{
-    var programOutcome = commandsIntArray
-    for (commandInput in commandsIntArray.indices step PROGRAM_SET) {
-        val commandCode = commandsIntArray[commandInput]
-        if (commandCode == TERMINATE) {
+fun initializeWithCommandInputs(commandsIntArray: IntArray): IntArray {
+    for (commandInput in commandsIntArray.indices step NUMBER_OF_VALUES) {
+        val instruction = commandsIntArray[commandInput]
+        if (instruction == TERMINATE) {
             break
         }
-        //TODO can I refactor this method and fuse it with runGravityAssistProgram?
-        // Is it possible to operate on the sliced portion?
-        programOutcome = runGravityAssistProgram(commandsIntArray, commandCode, commandsIntArray.slice(commandInput+1 .. commandInput+3))
-    }
-    return programOutcome
-}
-
-//TODO Can I/Should I refactor commandCodeInputs into 3 separate vals?
-fun runGravityAssistProgram (commandsIntArray: IntArray, commandCode:Int, commandCodeInputs : List<Int>) : IntArray{
-    when (commandCode) {
-        ADDITION -> {
-            commandsIntArray[commandCodeInputs[2]] =
-                (commandsIntArray[commandCodeInputs[0]] + commandsIntArray[commandCodeInputs[1]])
-        }
-        MULTIPLY -> {
-            commandsIntArray[commandCodeInputs[2]] =
-                (commandsIntArray[commandCodeInputs[0]] * commandsIntArray[commandCodeInputs[1]])
+        try {
+            val (param1, param2, param3) = commandsIntArray.slice(commandInput + 1..commandInput + 3)
+            runGravityAssistProgram(commandsIntArray, instruction, param1, param2, param3)
+        } catch (e: ArrayIndexOutOfBoundsException) {
+            break
         }
     }
     return commandsIntArray
 }
 
+fun runGravityAssistProgram(commandsIntArray: IntArray, commandCode: Int, param1: Int, param2: Int, param3: Int) {
+    when (commandCode) {
+        ADDITION -> commandsIntArray[param3] = (commandsIntArray[param1] + commandsIntArray[param2])
+        MULTIPLY -> commandsIntArray[param3] = (commandsIntArray[param1] * commandsIntArray[param2])
+    }
+}
+
+fun determineNounAndVerb(commandInputs: IntArray): Int {
+    for (noun in 0 until 99) {
+        for (verb in 0 until 99) {
+            val copyOfCommandInput = commandInputs.copyOf()
+            copyOfCommandInput[1] = noun
+            copyOfCommandInput[2] = verb
+            initializeWithCommandInputs(copyOfCommandInput)
+            if (copyOfCommandInput[0] == OBJECTIVE) {
+                return 100 * noun + verb
+            }
+        }
+    }
+    return -1
+}
